66260434c6c1472e704212ec4f547590a0e24fec
Paulo <prsilvapontes@gmail.com>
2022-04-05 18:56:53 -0300

    Fix TelaMenu

--- lib/view/telamenu.dart
+++ lib/view/telamenu.dart
@@ -24,87 +24,103 @@ class _MenuState extends State<Menu> {
               decoration: const BoxDecoration(
                 color: Colors.white30,
               ),
-              child: ListTile(
-                  leading: usuarioAutenticado.avatar! == ""
-                      ? const Icon(
-                          Icons.account_circle,
-                          color: Colors.blue,
-                        )
-                      : CircleAvatar(
-                          backgroundImage:
-                              NetworkImage(usuarioAutenticado.avatar!),
-                          radius: 30,
-                        ),
-                  title: Text(
-                    usuarioAutenticado.nome!,
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  subtitle: Text(usuarioAutenticado.email!),
-                  trailing: Container(
-                    padding: const EdgeInsets.only(left: 20),
-                    width: 50,
-                    child: IconButton(
-                      icon: const Icon(
-                        Icons.close,
-                        color: Colors.blue,
+              child: Column(
+                children: [
+                  ListTile(
+                      leading: usuarioAutenticado.avatar! == ""
+                          ? const Icon(
+                              Icons.account_circle,
+                              color: Colors.blue,
+                            )
+                          : CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(usuarioAutenticado.avatar!),
+                              radius: 30,
+                            ),
+                      title: Text(
+                        usuarioAutenticado.nome!,
+                        style: const TextStyle(fontSize: 18),
                       ),
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
+                      subtitle: Text(usuarioAutenticado.email!),
+                      trailing: Container(
+                        padding: const EdgeInsets.only(left: 20),
+                        width: 50,
+                        child: IconButton(
+                          icon: const Icon(
+                            Icons.close,
+                            color: Colors.blue,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )
+                  ),
+                  SizedBox(height: 10,),
+                  const TextField(
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: "Buscar item do menu",
+                      //border: OutlineInputBorder(),
+                      suffixIcon: Icon(Icons.search)
                     ),
-                  ))),
+                  )
+                ],
+              )
+          ),
+          const Divider(color: Colors.black26),
           ListTile(
             onTap: () {
               Navigator.pushReplacementNamed(context, "/telainicio");
             },
-            leading: const Icon(Icons.home),
+            leading: const Icon(Icons.home, color: Colors.blue,),
             title: const Text("Início"),
           ),
           ListTile(
             onTap: () {},
-            leading: const Icon(Icons.shopping_cart),
+            leading: const Icon(Icons.shopping_cart, color: Colors.blue,),
             title: const Text("Carrinho"),
           ),
           ListTile(
             onTap: () {},
-            leading: const Icon(Icons.add),
+            leading: const Icon(Icons.add, color: Colors.blue,),
             title: const Text("Produtos"),
           ),
           ListTile(
             onTap: () {},
-            leading: const Icon(Icons.store),
+            leading: const Icon(Icons.store, color: Colors.blue,),
             title: const Text("Fornecedores"),
           ),
           ListTile(
             onTap: () {},
-            leading: const Icon(Icons.apartment),
+            leading: const Icon(Icons.apartment, color: Colors.blue,),
             title: const Text("Clientes"),
           ),
           ListTile(
             onTap: () {
               Navigator.pushNamed(context, "/telausuario");
             },
-            leading: const Icon(Icons.person),
+            leading: const Icon(Icons.person, color: Colors.blue,),
             title: const Text("Usuários"),
           ),
           ListTile(
             onTap: () {
               Navigator.pushNamed(context, "/telaapi");
             },
-            leading: const Icon(Icons.api),
+            leading: const Icon(Icons.api, color: Colors.blue,),
             title: const Text("Api"),
           ),
           ListTile(
             onTap: () {},
-            leading: const Icon(Icons.settings),
+            leading: const Icon(Icons.settings, color: Colors.blue,),
             title: const Text("Configurações"),
           ),
-          const Divider(),
+          const Divider(color: Colors.black26),
           ListTile(
             onTap: () {
               Navigator.pushReplacementNamed(context, "/");
             },
-            leading: const Icon(Icons.logout),
+            leading: const Icon(Icons.logout, color: Colors.blue,),
             title: const Text("Sair"),
           ),
         ],
