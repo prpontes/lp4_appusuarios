1f2f94954971a86bfb0b19c4b1e53a5d839d42bd
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-04-14 14:00:00 -0300

    feat: undo delete

--- lib/components/details_user_dialog.dart
+++ lib/components/details_user_dialog.dart
@@ -1,5 +1,7 @@
 import 'package:lp4_appusuarios/model/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:lp4_appusuarios/provider/usuario_provider.dart';
+import 'package:provider/provider.dart';
 
 class DetailsUserDialog extends StatefulWidget {
   final Usuario usuario;
@@ -10,6 +12,14 @@ class DetailsUserDialog extends StatefulWidget {
 }
 
 class _DetailsUserDialogState extends State<DetailsUserDialog> {
+  late UsuarioProvider usuarioProvider;
+
+  @override
+  void initState() {
+    super.initState();
+    usuarioProvider = Provider.of<UsuarioProvider>(context, listen: false);
+  }
+
   @override
   Widget build(BuildContext context) {
     final Usuario usuario = widget.usuario;
@@ -31,6 +41,30 @@ class _DetailsUserDialogState extends State<DetailsUserDialog> {
             Tab(icon: Icon(Icons.settings)),
           ]),
           title: const Text("Detalhe usuário"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.edit),
+              onPressed: () {
+                // Navigator.pushNamed(context, '/edit_user', arguments: usuario);
+              },
+            ),
+            IconButton(
+              onPressed: () async {
+                await usuarioProvider.deletarUsuario(usuario);
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  action: SnackBarAction(
+                    label: 'Desfazer',
+                    onPressed: () async {
+                      await usuarioProvider.inserirUsuario(usuario);
+                    },
+                  ),
+                  content: const Text('Usuário deletado com sucesso!'),
+                ));
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.delete),
+            ),
+          ],
         ),
         body: TabBarView(children: [
           Padding(
