3ef8ab2166218465b920113ce1aa5e138f1097e5
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-04-14 07:56:01 -0300

    chore: comment unused lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 import 'package:lp4_appusuarios/api/api.dart';
 import 'package:lp4_appusuarios/api/detalhe_album.dart';
 import 'package:lp4_appusuarios/provider/provider_usuario.dart';
-import 'package:lp4_appusuarios/services/auth_service.dart';
-import 'package:lp4_appusuarios/view/AuthCheck.dart';
+// import 'package:lp4_appusuarios/services/auth_service.dart';
+// import 'package:lp4_appusuarios/view/AuthCheck.dart';
 import 'package:lp4_appusuarios/view/detalhe_usuario.dart';
 import 'package:lp4_appusuarios/view/tela_inicio.dart';
 import 'package:lp4_appusuarios/view/tela_login.dart';
@@ -21,7 +21,7 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => AuthService()),
+        // ChangeNotifierProvider(create: (context) => AuthService()),
         ChangeNotifierProvider(create: (context) => UsuarioModel()),
       ],
       child: MaterialApp(
@@ -32,9 +32,9 @@ void main() async {
           "/": (context) => const TelaLogin(),
           "/telainicio": (context) => const TelaInicio(),
           "/telausuario": (context) => const TelaUsuario(),
-          "/telaapi":(context) => const Api(),
-          "/detalhealbum" : (context) => const DetalheAlbum(),
-          "/detalheusuario" : (context) => const TelaDetalheUsuario(),
+          "/telaapi": (context) => const Api(),
+          "/detalhealbum": (context) => const DetalheAlbum(),
+          "/detalheusuario": (context) => const TelaDetalheUsuario(),
         },
       ),
     ),
