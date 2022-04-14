237a0fbe2a4971bb9daf5932e09a979e27156331
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-04-14 07:25:04 -0300

    chore: comment

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:provider/provider.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Inicializa o singleton do banco de dados
   await DatabaseSingleton.startDatabase();
 
   runApp(
