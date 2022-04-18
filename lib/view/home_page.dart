e6cff4cd6a8171a8326968084e012ffa6d466f49
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-04-18 13:43:06 -0300

    chore: improve home page buttons visual

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -24,66 +24,77 @@ class _TelaInicioState extends State<TelaInicio> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(20),
+                SizedBox(
                   width: 300,
                   height: 100,
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.store,
-                        size: 50,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Text(
-                        "Fornecedores",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(10)),
-                  padding: const EdgeInsets.only(left: 50, top: 10),
-                  width: 300,
-                  height: 100,
-                  child: Row(
-                    children: const [
-                      Icon(
-                        Icons.apartment,
-                        size: 50,
-                      ),
-                      SizedBox(
-                        width: 15,
-                      ),
-                      Text(
-                        "Clientes",
-                        style: TextStyle(fontSize: 30),
-                      ),
-                    ],
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(context, "/telausuario");
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlue,
-                      borderRadius: BorderRadius.circular(10),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/telausuario");
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
                     ),
-                    padding: const EdgeInsets.only(left: 40, top: 10),
-                    width: 300,
-                    height: 100,
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.store,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "Usuários",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/telausuario");
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.apartment,
+                          size: 50,
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(
+                          "Clientes",
+                          style: TextStyle(fontSize: 30),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/telausuario");
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.purple),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: const [
                         Icon(
                           Icons.person,
@@ -100,6 +111,35 @@ class _TelaInicioState extends State<TelaInicio> {
                     ),
                   ),
                 )
+                // GestureDetector(
+                //   onTap: () {
+                //     Navigator.pushNamed(context, "/telausuario");
+                //   },
+                //   child: Container(
+                //     decoration: BoxDecoration(
+                //       color: Colors.lightBlue,
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //     padding: const EdgeInsets.only(left: 40, top: 10),
+                //     width: 300,
+                //     height: 100,
+                //     child: Row(
+                //       children: const [
+                //         Icon(
+                //           Icons.person,
+                //           size: 50,
+                //         ),
+                //         SizedBox(
+                //           width: 15,
+                //         ),
+                //         Text(
+                //           "Usuários",
+                //           style: TextStyle(fontSize: 30),
+                //         ),
+                //       ],
+                //     ),
+                //   ),
+                // )
               ]),
         ));
   }
