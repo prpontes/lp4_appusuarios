a8d8265929c7bf76c3ee2f96df1adad2e18bbf54
Lino Mendes <lino.mendes.fisico@gmail.com>
2022-04-25 19:57:46 -0300

    feat: adicionado model fornecedores e provider

---
+++ lib/provider/fornecedores_provider.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/widgets.dart';
+import 'package:lp4_appusuarios/model/fornecedores.dart';
+import 'package:lp4_appusuarios/singletons/database_singleton.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/material.dart';
+
+class FornecedoresProvider extends ChangeNotifier {
+  Database db = DatabaseSingleton.instance.db;
+  String nomeTabela = "fornecedor";
+
+  List<Fornecedor> fornecedores = [];
+
+  FornecedoresProvider() {
+    debugPrint("FornecedoresProvider()");
+    // listarFornecedores();
+  }
+
+  Future<List<Fornecedor>> listaFornecedores() async {
+    List lista = await db.query(nomeTabela);
+
+    fornecedores = List.generate(lista.length, (index) {
+      return Fornecedor(
+        id: lista[index]["id"],
+        cnpj: lista[index]["cnpj"],
+        razaoSocial: lista[index]["razaoSocial"],
+        email: lista[index]["email"],
+        telefone: lista[index]["telefone"],
+        login: lista[index]["login"],
+        senha: lista[index]["senha"],
+        produto: lista[index]["produto"],
+        quantidade: lista[index]["quantidade"],
+
+      );
+    });
+    notifyListeners();
+    return fornecedores;
+  }
+
+  Future<Fornecedor?> consultarLoginFornecedor(String login, String senha) async {
+    List resultado = await db.query(nomeTabela,
+        where: "login = ? and senha = ?", whereArgs: [login, senha]);
+
+    if (resultado.isNotEmpty) {
+      return Fornecedor(
+          id: resultado[0]["id"],
+          cnpj: resultado[0]["cnpj"],
+          razaoSocial: resultado[0]["razaoSocial"],
+          email: resultado[0]["email"],
+          telefone: resultado[0]["telefone"],
+          login: resultado[0]["login"],
+          senha: resultado[0]["senha"],
+          produto: resultado[0]["produto"],
+          quantidade: resultado[0]["quantidade"]);
+    } else {
+      return null;
+    }
+  }
+
+  Future<int> inserirFornecedor(Fornecedor fornecedor) async {
+    int id = await db.insert(nomeTabela, fornecedor.toMap());
+    fornecedor.id = id;
+    fornecedores.add(fornecedor);
+    notifyListeners();
+    return id;
+  }
+
+  Future<int> editarUsuario(Fornecedor fornecedor) async {
+    int id = await db.update(nomeTabela, fornecedor.toMap(),
+        where: "id = ?", whereArgs: [fornecedor.id]);
+    debugPrint(id.toString());
+    notifyListeners();
+    return id;
+  }
+
+  Future<int> deletarUsuario(Fornecedor fornecedor) async {
+    int id =
+        await db.delete(nomeTabela, where: "id = ?", whereArgs: [fornecedor.id]);
+    fornecedores.remove(fornecedores);
+    notifyListeners();
+    return id;
+  }
+}
