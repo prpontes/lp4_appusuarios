92a343415a830fd765b9d10b0fc6ed81e8cc37b6
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-04-25 21:45:23 -0300

    Layout Vendas Estático Feito

---
+++ lib/test/sell_data.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:lp4_appusuarios/main.dart';
+
+class SellData {
+ static final getData = [
+   {
+     'name': 'Venda 01',
+     'id': 'km626733e568315',
+     'icon': Icons.attach_money,
+     'color': Colors.amber,
+   },
+     {
+     'name': 'Venda 02',
+     'id': 'RV62673e2746d9b',
+     'icon': Icons.attach_money,
+     'color': Colors.deepPurple,
+   },
+    {
+     'name': 'Venda 03',
+     'id': '3y62674009315d5',
+     'icon': Icons.attach_money,
+     'color': Colors.green,
+   },
+    {
+     'name': 'Venda 04',
+     'id': 'i26267400485c4f',
+     'icon': Icons.attach_money,
+     'color': Colors.indigo,
+   },
+ ];
+
+}
