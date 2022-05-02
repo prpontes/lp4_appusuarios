f749f18dcbfb364133f1dc564356021aeff0959a
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-05-02 20:10:25 -0300

    Sincronizando Projeto

--- lib/components/details_sell_dialog.dart
+++ lib/components/details_sell_dialog.dart
@@ -1,80 +1,62 @@
-
 import 'package:flutter/material.dart';
-import '../view/sales.page.dart';
 
-
-class detalheVendas extends StatefulWidget {
- 
-
-   detalheVendas({ Key? key,}) : super(key: key);
+class DetalheVendas extends StatefulWidget {
+  const DetalheVendas({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  _detalheVendasState createState() => _detalheVendasState();
+  _DetalheVendasState createState() => _DetalheVendasState();
 }
 
-class _detalheVendasState extends State<detalheVendas> {
+class _DetalheVendasState extends State<DetalheVendas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-        title: Text("Detalhes da Venda"),
-        actions: []
-        ),
-       
+      appBar: AppBar(title: Text("Detalhes da Venda"), actions: []),
       body: Column(children: [
-      
-                        const Icon(
-                            Icons.price_check_outlined,
-                            color: Colors.blue,
-                            size: 150,
-                          ),
-                       
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Nome do Produto: ",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          
-                        ],
-                      ),
-                    ),
-                    
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Cliente: ",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                         
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text(
-                            "Fornecedor: ",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                        
-                        ],
-                      ),
-                    ),
-                   
+        const Icon(
+          Icons.price_check_outlined,
+          color: Colors.blue,
+          size: 150,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Nome do Produto: ",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Cliente: ",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Fornecedor: ",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
       ]),
-
     );
   }
 }
