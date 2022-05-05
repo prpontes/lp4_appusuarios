a3f9879e59c22b8e7e9b8f559a9594155327c3b3
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2022-05-05 18:39:55 -0300

    feat: add crud

--- lib/provider/shopping_cart_provider.dart
+++ lib/provider/shopping_cart_provider.dart
@@ -1,3 +1,54 @@
 import 'package:flutter/widgets.dart';
+import 'package:lp4_appusuarios/model/item_venda.dart';
 
-class ShoppingCart extends ChangeNotifier {}
+class ShoppingCart extends ChangeNotifier {
+  List<ItemVenda> items = [];
+
+  void add(ItemVenda item) {
+    items.add(item);
+    notifyListeners();
+  }
+
+  void remove(ItemVenda item) {
+    items.remove(item);
+    notifyListeners();
+  }
+
+  double get totalPrice {
+    return items.fold(0, (total, current) => total + current.price);
+  }
+
+  int get totalItems {
+    return items.fold(0, (total, current) => total + current.quantity);
+  }
+
+  void clear() {
+    items = [];
+    notifyListeners();
+  }
+
+  List<ItemVenda> get getItems {
+    return [...items];
+  }
+
+  void increment(ItemVenda item) {
+    item.quantity++;
+    notifyListeners();
+  }
+
+  void decrement(ItemVenda item) {
+    item.quantity--;
+    if (item.quantity == 0) {
+      remove(item);
+    }
+    notifyListeners();
+  }
+
+  void setQuantity(ItemVenda item, int quantity) {
+    item.quantity = quantity;
+    if (item.quantity == 0) {
+      remove(item);
+    }
+    notifyListeners();
+  }
+}
