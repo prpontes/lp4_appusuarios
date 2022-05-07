21489245a400cae751058372841ee0f2842b0dad
lhenriquedeveloper <sousarodriguesluishenrique@gmail.com>
2022-05-06 22:27:11 -0300

    query lista details venda

--- lib/provider/sell_provider.dart
+++ lib/provider/sell_provider.dart
@@ -35,7 +35,7 @@ class SellProvider extends ChangeNotifier {
   // listarItems
   Future<List<ItemVenda>> listItens() async {
     List lista = await db.rawQuery(
-        "SELECT usuario.nome as 'usuario' FROM usuario JOIN sell on (sell.idUser = usuario.id) where usuario.id = sell.idUser");
+        "SELECT itemVenda.quantity as quantidade, itemVenda.price as preco, product.name as produto, usuario.nome as nome from  usuario join sell on (sell.idUser = usuario.id)  join itemVenda on (itemVenda.id = sell.id) join product on ( product.id = itemvenda.idProduto) WHERE usuario.id = sell.idUser;");
     itens = List.generate(lista.length, (index) {
       return ItemVenda(
         usuario: lista[index]["usuario"],
